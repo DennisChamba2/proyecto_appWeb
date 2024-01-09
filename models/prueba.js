166dd00a137ddd5d80864a005eb0bfc6f546bc80
DennisChamba <dennis.chamba@live.com>
2024-01-08 19:25:46 -0500

    correccion de documento de pruebas de coneccion a BD

--- models/prueba.js
+++ models/prueba.js
@@ -1,4 +1,16 @@
-const {db} = require("./firebase")
+const db = require("./firebase");
 
+async function obtenerPlatillos() {
+    try {
+        // Obtener una referencia a la colección "platillos"
+        const platillosSnapshot = (await db.collection("platillos").get());
 
-console.log(db.collection("platillos"),get());
+        // Hacer algo con la referencia a la colección (por ejemplo, loggearla)
+        console.log(platillosSnapshot.docs[0].data()); // Accede a los documentos de la colección
+    } catch (error) {
+        console.error("Error al obtener platillos:", error);
+    }
+}
+
+// Llama a la función asincrónica
+obtenerPlatillos();
