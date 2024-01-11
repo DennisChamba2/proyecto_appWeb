be751c5572f746465d027b9053f6a755a7ba08b9
DennisChamba <dennis.chamba@live.com>
2024-01-11 07:01:31 -0500

    update 12 files

--- models/chatServer.js
+++ models/chatServer.js
@@ -1,69 +1,38 @@
-// const WebSocket = require('ws');
-// const https = require('https');
-// const express = require('express');
-// const path = require('path');
-// const fs = require('fs');
-
-
-// const app = express();
-
-// // SSL credentials
-// const server = https.createServer({
-//     cert: fs.readFileSync('../secrets/cert.pem'),
-//     key: fs.readFileSync('../secrets/key.pem')
-//   }, app);
-  
-
-// // const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server });
-
-// wss.on('connection', (ws) => {
-//     ws.on('message', (message) => {
-//         console.log(message);
-//         wss.clients.forEach((client) => {
-//             if (client !== ws && client.readyState === WebSocket.OPEN) {
-//                 client.send(message);
-//             }
-//         });
-//     });
-// });
-
-// app.use(express.static(path.join(__dirname, '../public/views')));
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/views", 'chat.hbs'));
-// });
-
-// server.listen(3000, () => {
-//     console.log('Servidor WebSocket en ejecución en el puerto 3000');
-// });
-
 const WebSocket = require('ws');
 const https = require('https');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
-function setupWebSocketServer(app) {
-  const server = https.createServer({
+
+const app = express();
+
+// SSL credentials
+const server = https.createServer({
     cert: fs.readFileSync(path.join(__dirname,'../secrets/cert.pem')),
     key: fs.readFileSync(path.join(__dirname,'../secrets/key.pem'))
   }, app);
-
-  const wss = new WebSocket.Server({ server });
-
-  wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-    //   console.log(message);
-      wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(message);
-        }
-      });
-    });
-  });
-
   
 
-  server.listen(3000);
-}
+// const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-module.exports = setupWebSocketServer;
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(message);
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+app.use(express.static(path.join(__dirname, '../public/views')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/views", 'chat.hbs'));
+});
+
+server.listen(3000, () => {
+    console.log('Servidor WebSocket en ejecución en el puerto 3000');
+});
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,60 +1,29 @@
-// const socket = new WebSocket('wss://localhost:3000');
+const socket = new WebSocket('wss://localhost:3000');
 
-// socket.addEventListener('open', (event) => {
-//     console.log('Conexión establecida con el servidor WebSocket');
-// });
-// //socket.onmessage = (event) => { };
-// socket.addEventListener('message', (event) => {
-//     event.data.arrayBuffer().then((data) => {
-//         const message = new TextDecoder('utf-8').decode(data);
-//         //console.log(message);
-//         const item = document.createElement('li');
-//         item.textContent = message;
-//         document.querySelector('#messages').appendChild(item);
-//     });
-// });
-
-// document.querySelector('#form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const message = document.querySelector('#input').value;
-//     if (socket.readyState === WebSocket.OPEN) {
-//         socket.send(message);
-//     }
-//     const item = document.createElement('li');
-//     item.textContent = message;
-//     document.querySelector('#messages').appendChild(item);
-//     document.querySelector('#input').value = '';
-//     document.querySelector('#input').focus();
-// });
-
-document.addEventListener('DOMContentLoaded', () => {
-    const socket = new WebSocket('wss://localhost:8000'); // Ajusta la URL según tu configuración
-
-    socket.addEventListener('open', (event) => {
-        console.log('Conexión establecida con el servidor WebSocket');
-    });
-
-    socket.addEventListener('message', (event) => {
-        const message = event.data;
+socket.addEventListener('open', (event) => {
+    console.log('Conexión establecida con el servidor WebSocket');
+});
+//socket.onmessage = (event) => { };
+socket.addEventListener('message', (event) => {
+    event.data.arrayBuffer().then((data) => {
+        const message = new TextDecoder('utf-8').decode(data);
+        //console.log(message);
         const item = document.createElement('li');
         item.textContent = message;
         document.querySelector('#messages').appendChild(item);
-    });
-
-    document.querySelector('#form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        const message = document.querySelector('#input').value;
-        if (socket.readyState === WebSocket.OPEN) {
-            socket.send(message);
-        }
-        const item = document.createElement('li');
-        item.textContent = message;
-        document.querySelector('#messages').appendChild(item);
-        document.querySelector('#input').value = '';
-        document.querySelector('#input').focus();
-    });
-
-    socket.addEventListener('close', (event) => {
-        console.log('Conexión cerrada:', event);
     });
 });
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = document.querySelector('#input').value;
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(message);
+    }
+    const item = document.createElement('li');
+    item.textContent = message;
+    document.querySelector('#messages').appendChild(item);
+    document.querySelector('#input').value = '';
+    document.querySelector('#input').focus();
+});
+
